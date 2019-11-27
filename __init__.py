f1cf41b0bd9f263e66db3a5e95543fb8997a38f8
Andrew Rowe <andrew.rowe@qbe.com>
2019-11-27 15:11:57 +1100

    Fix stack over flow bug.  Add comma for multiple keys.  Refactor a bit.

--- __init__.py
+++ __init__.py
@@ -16,6 +16,11 @@ class PermissiveDict(dict):
 
     """
 
+    def __init__(self, kwargs):
+        self.__map_fields = {}
+        self.__default_returned = False
+        return super().__init__(kwargs)
+
     def __getattr__(self, name):
         try:
             value = super().__getattr__(name)
@@ -32,41 +37,40 @@ class PermissiveDict(dict):
 
     def __contains__(self, item):
         self.get(item)
-        return not self.__default_returned__
+        return not self.__default_returned
 
     def get(self, key, default=''):
         """
         return the value in dict d which matches the key
         using a str and upper comparision.  Tries normal get
-        first.
+        first.  Then a miscellany of common punctuation characters
 
-        :param key: key to return, converts to str
-        :param default: default value if not found
+        Use a comma to try a number of different keys for the same thing.
+
+        :param key: key to find, converts to str
+        :param default: default value if not found.  NOTE: unlike Python uses '' as the default
         :return: the value found or default if not found
         """
-        self.__default_returned__ = False
+        self.__default_returned = False
         value = super().get(key)
         if value:
             return value
 
-        requested_key = str(key).upper().strip()
-        for k, v in self.items():
-            match_key = str(k).upper().strip()
-            if match_key == requested_key:
-                return v
-            if match_key.replace('_', ' ') == requested_key:
-                return v
-            if match_key.replace(' ', '_') == requested_key:
-                return v
-            if match_key.replace(' ', '-') == requested_key:
-                return v
-            if match_key.replace('-', ' ') == requested_key:
-                return v
-        value = self.__get_map_value__(key, default)
-        if value != default:
-            return value
-        self.__default_returned__ = True
-        return default
+        requested_keys = str(key).upper().strip()
+        for requested_key in [r for r in requested_keys.split(',') if len(r) > 0]:
+            for r in [('', ''), ('_', ' '), (' ', '_'), (' ', '-'), ('-', ' '), (' ', '.'), ('.', ' ')]:
+                value = super().get(requested_key.replace(*r))
+                if value:
+                    # print(f'short cut: {requested_key}')
+                    return value
+
+                for k, v in self.items():
+                    match_key = str(k).replace(*r).upper().strip()
+                    if match_key == requested_key:
+                        # print(f'iterated: {requested_key}')
+                        return v
+
+        return self.__get_map_value__(key, default)
 
     @classmethod
     def convert_list(cls, items):
@@ -88,6 +92,7 @@ class PermissiveDict(dict):
         """
         map_keys = self.__map_fields.get(key)
         if not map_keys:
+            self.__default_returned = True
             return default
 
         for k in map_keys:
@@ -95,30 +100,30 @@ class PermissiveDict(dict):
             if value:
                 return value
 
+        self.__default_returned = True
         return default
 
 
 if __name__ == '__main__':
-    d = PermissiveDict({'A': 1, 'A B': 2, 'b': 3, 4: 4, 'lab': 'woof!'})
-    d.set_map(dict(dog=['puppy', 'pupper', 'lab']))
+    old_d = dict({'A': 1, 'A B': 2, 'b': 3, 4: 4, 'lab': 'woof!'})
+    d = PermissiveDict(old_d)
     assert d.get('a') == d.get('A') == 1
-    assert d.get('A_b') == 2
+    assert d.a_b == d.get('A_b') == 2
     assert d.get('B') == 3
-    assert d.a == 1
-    assert d.A == 1
-    assert d.a_b == 2
+    assert d.a == d.A == 1
     assert d.q == ''
-    assert d['a'] == 1
-    assert d['A'] == 1
-    assert d['A '] == 1
-    assert d[' a '] == 1
-    assert d[' a-b '] == 2
-    assert d['A-b '] == 2
-    assert d[' a_b '] == 2
+    assert d['a'] == d['A'] == d['A '] == d[' a '] == 1
+    assert d[' a-b '] == d['A-b '] == d[' a_b '] == d[' a.b '] == 2
     assert ' a_b ' in d
     assert 'A' in d
     assert 'a' in d
     assert 'q' not in d
     assert '4' in d
+    assert 4 in d
     assert d.get(4) == d.get('4') == 4
+    assert d.flong == d['flong'] == d.get('flong') == d.get('pling,plong,,') == ''
+    assert d['flong,A'] == d.get('plok,a') == d.a
+
+    # dog key could resolve using the dog, puppy, pupper or lab key
+    d.set_map(dict(dog=['puppy', 'pupper', 'lab']))
     assert d.dog == d['dog'] == d.get('dog') == 'woof!'
